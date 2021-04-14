95102bde730d72efe18e65f6bea8b17b6b6cd91a
Olli Janatuinen <olli.janatuinen@gmail.com>
2021-04-14 19:45:24 +0300

    Drop old OS base

--- images/02-acpid/etc/acpi/suspend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if [ "$3" = "close" ]; then
-    echo -n "mem" > /sys/power/state
-fi
--- images/02-syslog/usr/bin/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /usr/share/logrotate/logrotate.d/* /etc/logrotate.d
-
-exec /usr/bin/ros entrypoint "$@"
